8dfe0fb1fda670c00b0059d5041d9cb64ea07ecd
HenniePenny <h.kressner@hotmail.de>
2022-08-14 22:42:57 +0200

    added chalk v4.1.0

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const inquirer = require("inquirer");
+const fs = require("fs");
+const chalk = require("chalk");
 const Employee = require("./lib/Employee");
 const Engineer = require("./lib/Engineer");
 const Manager = require("./lib/Manager");
