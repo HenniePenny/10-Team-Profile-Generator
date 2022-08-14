e4eaa8b176fea326b33410a08f9278c943909575
HenniePenny <h.kressner@hotmail.de>
2022-08-14 16:56:35 +0200

    add employee test

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -9,14 +9,64 @@ describe("Employee", () => {
       expect("id" in obj).toEqual(true);
       expect("email" in obj).toEqual(true);
     });
-
-    it("should set 'name' when created", () => {
-      const name = "Marie";
-
-      const obj = new Employee(name);
-
-      expect(obj.name).toEqual(name);
-    });
   });
-  //! Create separate tests for getName, getID and getEmail
 });
+
+describe("methodsForEmployee", () => {
+  const name = "Marie";
+  const id = "5";
+  const email = "marie@test.com";
+
+  it("should return the employee's name", () => {
+    const obj = new Employee(name, id, email);
+    expect(obj.getName()).toEqual(name);
+  });
+
+  it("should return the employee's id", () => {
+    const obj = new Employee(name, id, email);
+    expect(obj.getId()).toEqual(id);
+  });
+
+  it("should return the employee's email address", () => {
+    const obj = new Employee(name, id, email);
+    expect(obj.getEmail()).toEqual(email);
+  });
+
+  it("should return the employee's role", () => {
+    const obj = new Employee(name, id, email);
+    expect(obj.getRole()).toEqual("Employee");
+  });
+});
+
+//!Keeping older code version as reference
+// describe("getName()", () => {
+//   it("should return the employee's name", () => {
+//     const name = "Marie";
+//     const id = "5";
+//     const email = "marie@test.com";
+//     const obj = new Employee(name, id, email);
+//     expect(obj.name).toEqual(name);
+//   });
+// });
+
+// describe("getID", () => {
+//   it("should return the employee's id", () => {
+//     const name = "Marie";
+//     const id = "5";
+//     const email = "marie@test.com";
+//     const obj = new Employee(name, id, email);
+//     expect(obj.id).toEqual(id);
+//   });
+// });
+
+// describe("getEmail", () => {
+//   it("should return the employee's email address", () => {
+//     const name = "Marie";
+//     const id = "5";
+//     const email = "marie@test.com";
+//     const obj = new Employee(name, id, email);
+//     expect(obj.email).toEqual(email);
+//   });
+// });
+
+//test for role
--- index.js
+++ index.js
@@ -1,2 +1,51 @@
 const inquirer = require("inquirer");
-const Employee = 
+const Employee = require("./lib/Employee");
+// const Engineer =
+// const Manager =
+// const Intern =
+
+//! const questionsManager - start with manager questions, then loop over the other employees
+const questions = [
+  {
+    name: "position",
+    type: "list",
+    message: "Please choose your team member's position or quit.",
+    choices: ["Manager", "Engineer", "Intern", "quit"],
+  },
+];
+
+const questionsEngineer = [
+  {
+    name: "nameEngineer",
+    type: "input",
+    message: "Please type in the Engineer's name",
+  },
+];
+
+// const questionsIntern
+
+let inProgress = true;
+
+const init = () => {
+  inquirer.prompt(questions).then((answers) => {
+    console.log(answers);
+    switch (answers.position) {
+      case "Engineer":
+        inquirer.prompt(questionsEngineer).then((answersEngineer) => {
+          console.log(answersEngineer);
+        });
+        break;
+
+      case "quit":
+        inProgress = false;
+        break;
+
+      default:
+        inProgress = false;
+        break;
+    }
+  });
+};
+init();
+
+//!do a while loop here ???
