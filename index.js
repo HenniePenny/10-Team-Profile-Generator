e3e077430b6e70a28c05a6a007c38c74bfe3298f
HenniePenny <h.kressner@hotmail.de>
2022-08-15 21:47:23 +0200

    add question arrays, add empty generateHTML

--- index.js
+++ index.js
@@ -5,26 +5,84 @@ const Employee = require("./lib/Employee");
 const Engineer = require("./lib/Engineer");
 const Manager = require("./lib/Manager");
 const Intern = require("./lib/Intern");
+const generateHTML = require("./lib/GenerateHTML");
 
-//! const questionsManager - start with manager questions, then loop over the other employees
-const questions = [
+//question or the Manager is the first set of questions, asked only once according to acceptance criteria
+const questionsManager = [
   {
-    name: "position",
-    type: "list",
-    message: "Please choose your team member's position or quit.",
-    choices: ["Manager", "Engineer", "Intern", "quit"],
+    type: "input",
+    name: "nameManager",
+    message: chalk.green("Please type in the Manager's name:"),
+  },
+  {
+    type: "input",
+    name: "idManager",
+    message: chalk.green("Please type in the Manager's employee ID number:"),
+  },
+  {
+    type: "input",
+    name: "emailManager",
+    message: chalk.green("Please type in the Manager's email address:"),
+  },
+  {
+    type: "input",
+    name: "officeNoManager",
+    message: chalk.green("Please type in the Manager's office number:"),
   },
 ];
 
+//choice to make after adding manager, loop back to this question after engineer or intern
+const questionsChoice = [
+  {
+    type: "list",
+    name: "addColleague",
+    message: chalk.yellow("Please add another team member or quit."),
+    choices: ["Engineer", "Intern", "quit"],
+  },
+];
+
+//questions for engineer, should go back to questionsChoice afterwards
 const questionsEngineer = [
   {
-    name: "nameEngineer",
     type: "input",
-    message: "Please type in the Engineer's name",
+    name: "nameEngineer",
+    message: chalk.magenta("Please type in the Engineer's name:"),
+  },
+  {
+    type: "input",
+    name: "idEngineer",
+    message: chalk.magenta("Please type in the Engineer's ID number:"),
+  },
+  {
+    type: "input",
+    name: "emailEngineer",
+    message: chalk.magenta("Please type in the Engineer's email address:"),
+  },
+  {
+    type: "input",
+    name: "github",
+    message: chalk.magenta("Please type in the Engineer's GitHub username:"),
   },
 ];
 
-// const questionsIntern
+//questions for intern, should go back to questionsChoice afterwards
+const questionsIntern = [
+  {
+    type: "input",
+    name: "nameIntern",
+    message: chalk.blue("Please type in the Intern's name:"),
+  },
+  {
+    type: "input",
+    name: "idIntern",
+    message: chalk.blue("Please type in the Intern's ID number:"),
+  },
+  {
+    type: "input",
+    name: "school",
+    message: chalk.blue("Please type in the Intern's school:"),
+  },
+];
 
 let inProgress = true;
 
